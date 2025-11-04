36f094567bd5421f3806896db00bbdb7cb0f9a17
Nelson-Paris <nelson.paris.pro@gmail.com>
2025-11-04 21:44:14 +0100

    test avec vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
 import './styles/index.scss'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename={import.meta.env.BASE_URL}>
-  <App/>
-</BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
